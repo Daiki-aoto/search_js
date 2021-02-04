cd4dcc8c037cad6bb17df35c5aab48da0eed9c6f
Daiki-aoto <77034532+Daiki-aoto@users.noreply.github.com>
2021-02-04 09:25:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
 $(function() {
-    var $searchInput = $('#search'); // 入力エリア
-    var $searchElem = $('.js-extraction'); // 絞り込む要素
-    var excludedClass = 'is-excluded'; // 絞り込み対象外の要素に付与するclass
- 
-    // 絞り込み処理
+    var $searchInput = $('#search'); 
+    var $searchElem = $('.js-extraction');
+    var excludedClass = 'is-excluded';
+    
+  
     function extraction() {
-        var keywordArr = $searchInput.val().toLowerCase().replace('　', ' ').split(' '); // 入力文字列を配列に格納
-        $searchElem.removeClass(excludedClass).show();// 現在非表示にしているリストを表示する
+        var keywordArr = $searchInput.val().toLowerCase().replace('　', ' ').split(' '); 
+        $searchElem.removeClass(excludedClass).show();
         for (var i = 0; i < keywordArr.length; i++) {
             for (var j = 0; j < $searchElem.length; j++) {
                 var thisString = $searchElem.eq(j).text().toLowerCase();
-                if(thisString.indexOf(keywordArr[i]) == -1) { // 入力文字列と一致する文字列がない場合
-                    $searchElem.eq(j).addClass(excludedClass); // 絞り込み対象外のclass付与
+                if(thisString.indexOf(keywordArr[i]) == -1) { 
+                    $searchElem.eq(j).addClass(excludedClass); 
                 }
             }
         }
-        $('.' + excludedClass).hide(); // 絞り込み対象外の要素の非表示
+        $('.' + excludedClass).hide(); 
     }
  
     $searchInput.on('load keyup blur', function() {
